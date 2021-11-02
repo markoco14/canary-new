6953f86164ee06aba37c2a4419d4a747052bc065
Mark O <mark.oconnor14@gmail.com>
2021-11-02 21:52:41 +0700

    add main page and new teacher signup page

---
+++ index.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<header>
+		
+		<h1>Canary Bank</h1>
+		<nav><ul>
+			<li>Home</li>
+			<li>Signup</li>
+			<li>Login</li>
+			<li>About</li>
+			<li>Contact</li>
+		</ul></nav>
+	</header>
+	<main>
+		<h2>Welcome to the Canary Bank</h2>
+		<div>
+			<h3>Teachers</h3>
+			<p>First time here? You need to make teacher profile before you can start creating classes and registering students.</p>
+			<a href="teacher-signup.php">Make Teacher Profile</a>
+			<p>Already a member?</p>
+			<a href="#">Login</a>
+		</div>
+	</main>
+</body>
+</html>
---
+++ teacher-signup.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<header>
+		
+		<h1>Canary Bank</h1>
+		<nav><ul>
+			<li><a href="index.php">Home</a></li>
+			<li>Signup</li>
+			<li>Login</li>
+			<li>About</li>
+			<li>Contact</li>
+		</ul></nav>
+	</header>
+	<main>
+		
+		<h1>Welcome New Teachers</h1>
+		<form action="">
+			<input type="text">
+			<input type="text">
+			<input type="password">
+			<input type="password">
+			<button type="submit" name="submit">Sign Up</button>
+		</form>
+	</main>
+</body>
+</html>
